f0e0011bb0310e3ce3e6e024106c44f31b68ae1f
Viet Pham <vietpham2017@gmail.com>
2025-05-01 01:37:55 -0500

    progress

--- app/(app)/my-programs/exercise-form.tsx
+++ app/(app)/my-programs/exercise-form.tsx
@@ -19,6 +19,7 @@ import {
 } from "@/components/ui/select";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useIsMobile } from "@/hooks/use-mobile";
+import ErrorAlert from "@/components/alerts/error-alert";
 
 export function ExerciseForm({
   onAddExercise,
@@ -51,6 +52,12 @@ export function ExerciseForm({
       return;
     }
 
+    // Validate min and max reps
+    if (minReps > maxReps) {
+      setError("Minimum reps cannot be greater than maximum reps");
+      return;
+    }
+
     // Create the exercise object
     const newExercise: CreateExercise = {
       exerciseName,
@@ -77,114 +84,118 @@ export function ExerciseForm({
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div className="space-y-2">
-        <Label htmlFor="exercise-name">Exercise Name</Label>
-        <Input
-          id="exercise-name"
-          placeholder="e.g., Bench Press"
-          value={exerciseName}
-          onChange={(e) => setExerciseName(e.target.value)}
-          className="text-sm"
-        />
-      </div>
-      <div className="w-full flex flex-col space-y-2">
-        <Label htmlFor="exercise-type">Body Part</Label>
-        <Select value={bodyPart} onValueChange={(value) => setBodyPart(value)}>
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Select body part" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="chest">Chest</SelectItem>
-            <SelectItem value="shoulders">Shoulders</SelectItem>
-            <SelectItem value="triceps">Triceps</SelectItem>
-            <SelectItem value="back">Back</SelectItem>
-            <SelectItem value="biceps">Biceps</SelectItem>
-            <SelectItem value="forearms">Forearms</SelectItem>
-            <SelectItem value="quadriceps">Quadriceps</SelectItem>
-            <SelectItem value="hamstrings">Hamstrings</SelectItem>
-            <SelectItem value="glutes">Glutes</SelectItem>
-            <SelectItem value="abs">Abs</SelectItem>
-            <SelectItem value="adductors">Adductors</SelectItem>
-            <SelectItem value="abductors">Abductors</SelectItem>
-            <SelectItem value="calves">Calves</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="space-y-2 mb-5">
-        <Label>Laterality</Label>
-        <RadioGroup
-          className="flex flex-col sm:flex-row gap-4"
-          value={laterality}
-          onValueChange={(value) => setLaterality(value)}
+    <>
+      <ErrorAlert error={error} setError={setError} />
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-2">
+          <Label htmlFor="exercise-name">Exercise Name</Label>
+          <Input
+            id="exercise-name"
+            placeholder="e.g., Bench Press"
+            value={exerciseName}
+            onChange={(e) => setExerciseName(e.target.value)}
+            className="text-sm"
+          />
+        </div>
+        <div className="w-full flex flex-col space-y-2">
+          <Label htmlFor="exercise-type">Body Part</Label>
+          <Select
+            value={bodyPart}
+            onValueChange={(value) => setBodyPart(value)}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select body part" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="chest">Chest</SelectItem>
+              <SelectItem value="shoulders">Shoulders</SelectItem>
+              <SelectItem value="triceps">Triceps</SelectItem>
+              <SelectItem value="back">Back</SelectItem>
+              <SelectItem value="biceps">Biceps</SelectItem>
+              <SelectItem value="forearms">Forearms</SelectItem>
+              <SelectItem value="quadriceps">Quadriceps</SelectItem>
+              <SelectItem value="hamstrings">Hamstrings</SelectItem>
+              <SelectItem value="glutes">Glutes</SelectItem>
+              <SelectItem value="abs">Abs</SelectItem>
+              <SelectItem value="adductors">Adductors</SelectItem>
+              <SelectItem value="abductors">Abductors</SelectItem>
+              <SelectItem value="calves">Calves</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="space-y-2 mb-5">
+          <Label>Laterality</Label>
+          <RadioGroup
+            className="flex flex-col sm:flex-row gap-4"
+            value={laterality}
+            onValueChange={(value) => setLaterality(value)}
+          >
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="bilateral" id="bilateral" />
+              <Label htmlFor="bilateral">Bilateral</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="unilateral" id="unilateral" />
+              <Label htmlFor="unilateral">Unilateral</Label>
+            </div>
+          </RadioGroup>
+        </div>
+
+        <div
+          className={`flex justify-between gap-4 ${isMobile ? "flex-col" : ""}`}
         >
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="bilateral" id="bilateral" />
-            <Label htmlFor="bilateral">Bilateral</Label>
+          <div className="space-y-2 w-full">
+            <Label htmlFor="sets">Sets</Label>
+            <Input
+              id="sets"
+              type="number"
+              placeholder="Enter number of sets"
+              value={sets || ""}
+              onChange={(e) => setSets(Number.parseInt(e.target.value))}
+              className="flex-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none placeholder:text-sm text-sm"
+            />
           </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="unilateral" id="unilateral" />
-            <Label htmlFor="unilateral">Unilateral</Label>
+          <div className="space-y-2 w-full">
+            <Label htmlFor="min-reps">Min Reps</Label>
+            <Input
+              id="min-reps"
+              type="number"
+              placeholder="Enter min reps"
+              value={minReps || ""}
+              onChange={(e) => setMinReps(Number.parseInt(e.target.value))}
+              className="flex-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none placeholder:text-sm text-sm"
+            />
           </div>
-        </RadioGroup>
-      </div>
-
-      <div
-        className={`flex justify-between gap-4 ${isMobile ? "flex-col" : ""}`}
-      >
-        <div className="space-y-2 w-full">
-          <Label htmlFor="sets">Sets</Label>
-          <Input
-            id="sets"
-            type="number"
-            placeholder="Enter number of sets"
-            value={sets || ""}
-            onChange={(e) => setSets(Number.parseInt(e.target.value))}
-            className="flex-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none placeholder:text-sm text-sm"
-          />
+          <div className="space-y-2 w-full">
+            <Label htmlFor="max-reps">Max Reps</Label>
+            <Input
+              id="max-reps"
+              type="number"
+              placeholder="Enter max reps"
+              value={maxReps || ""}
+              onChange={(e) => setMaxReps(Number.parseInt(e.target.value))}
+              className="flex-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none placeholder:text-sm text-sm"
+            />
+          </div>
         </div>
-        <div className="space-y-2 w-full">
-          <Label htmlFor="min-reps">Min Reps</Label>
-          <Input
-            id="min-reps"
-            type="number"
-            placeholder="Enter min reps"
-            value={minReps || ""}
-            onChange={(e) => setMinReps(Number.parseInt(e.target.value))}
-            className="flex-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none placeholder:text-sm text-sm"
-          />
-        </div>
-        <div className="space-y-2 w-full">
-          <Label htmlFor="max-reps">Max Reps</Label>
-          <Input
-            id="max-reps"
-            type="number"
-            placeholder="Enter max reps"
-            value={maxReps || ""}
-            onChange={(e) => setMaxReps(Number.parseInt(e.target.value))}
-            className="flex-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none placeholder:text-sm text-sm"
-          />
-        </div>
-      </div>
 
-      {error && <p className="text-sm text-red-500">{error}</p>}
-
-      <div className="flex justify-end">
-        <Button
-          type="submit"
-          className="mt-4 w-fit"
-          disabled={
-            !exerciseName ||
-            !bodyPart ||
-            !laterality ||
-            !sets ||
-            !minReps ||
-            !maxReps
-          }
-        >
-          Add Exercise
-        </Button>
-      </div>
-    </form>
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            className="mt-4 w-fit"
+            disabled={
+              !exerciseName ||
+              !bodyPart ||
+              !laterality ||
+              !sets ||
+              !minReps ||
+              !maxReps
+            }
+          >
+            Add Exercise
+          </Button>
+        </div>
+      </form>
+    </>
   );
 }
