ad6b3a4c352578ab7ca6f8ac9ce6782ec4f06cb2
Viet Pham <vietpham2017@gmail.com>
2025-08-25 19:30:46 -0500

    progress

--- components/global/topbar.tsx
+++ components/global/topbar.tsx
@@ -43,10 +43,9 @@ export function TopBar({ className }: TopBarProps) {
   return (
     <header
       className={cn(
-        `sticky top-0 z-50 flex h-[65px] w-full items-center standalone:pt-14 standalone:h-[100px] ${
-          isMobile ? "" : "border-b"
-        },
-         bg-background px-4 transition-opacity duration-500 ease-in-out`,
+        `sticky top-0 z-50 flex h-[65px] w-full items-center standalone:pt-14 standalone:h-[100px] bg-background px-4 transition-opacity duration-500 ease-in-out ${
+          isMobile ? "border-none" : "border-b"
+        }`,
         {
           "opacity-0 pointer-events-none": isMobile && !isVisible,
           "opacity-100 pointer-events-auto": !isMobile || isVisible,
