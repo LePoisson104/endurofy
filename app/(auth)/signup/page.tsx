97ccf9bc85c9c99d7b209df11076e24c3ad221f5
Viet Pham <vietpham2017@gmail.com>
2025-12-29 22:25:00 -0600

    progress

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -12,7 +12,6 @@ import { useSignupMutation } from "@/api/auth/auth-api-slice";
 import { Loader2 } from "lucide-react";
 import { validatePasswordField } from "@/helper/password-validator";
 import { toast } from "sonner";
-import ContinueWithGoogle from "@/components/buttons/continue-with-google";
 
 interface FormErrors {
   firstName: string;
@@ -190,9 +189,6 @@ export default function Signup() {
               Get started with Endurofy
             </h1>
           </div>
-
-          <ContinueWithGoogle />
-          <div className="w-full border-b mt-4 mb-4" />
 
           <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
             <div className="flex gap-2">
